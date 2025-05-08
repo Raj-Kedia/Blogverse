83678089462867d1fd06f7ca31b51254a0661614
Raj Kedia <rajramkola786@gmail.com>
2025-05-08 23:09:26 +0530

    update settings.py for deployment on railway

--- blogverse/settings.py
+++ blogverse/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 SECRET_KEY = os.environ.get('SECRET_KEY', "l=rlgjubv9^b)1*zwxb@hwsdu&k_r=(xaz$g!d1vd#+@md+mn^")
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-CSRF_TRUSTED_ORIGINS = ["*"]
+DEBUG = False
+CSRF_TRUSTED_ORIGINS = ["https://blogverse-production-822b.up.railway.app/"]
 ALLOWED_HOSTS = ["*"] 
 
 # Application definition
