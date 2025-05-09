e35570757a270ebeb7fcf14677ed285a35c381e3
Raj Kedia <rajramkola786@gmail.com>
2025-05-09 23:29:16 +0530

    Switched to PostgreSQL

--- blogverse/settings.py
+++ blogverse/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "blogverse.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('PGDATABASE', ),
+        'NAME': os.getenv('PGDATABASE'),
         'USER': os.getenv('PGUSER'),
         'PASSWORD': os.getenv('PGPASSWORD'),
         'HOST': os.getenv('PGHOST'),
